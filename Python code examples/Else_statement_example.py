b3a062d45b95a9d9c237f5a4d18d9e570450b10a
Irving Innes <I.Innes@albynschool.org>
2024-11-28 12:50:23 +0000

    .

---
+++ Python code examples/Else_statement_example.py
@@ -0,0 +1,6 @@
+ticket = str(input("Do you whisp to buy a ticket?"))
+if ticket == "Y":
+    print("Welcome to the tciket office")
+else: 
+    print("We are sorry to hear that")
+    print("NEVER COME BACK")
---
+++ Python code examples/if statement example.py
@@ -0,0 +1,3 @@
+ticket = str(input("Do you whisp to buy a ticket?"))
+if ticket == "y":
+    print("Welcome to the tciket office")
